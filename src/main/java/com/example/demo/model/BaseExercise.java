745ae7cc4a74a5fa1d057a88d379024c4a252390
shanjib <ishanjib@gmail.com>
2025-11-06 18:02:40 -0500

    separate Exercise and TrackedExercise into different tables via BaseExercise class

---
+++ src/main/java/com/example/demo/model/BaseExercise.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
+public abstract class BaseExercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int sets;
+    private int reps;
+    private double weightIncrement;
+    private double initialWeight;
+
+    @Enumerated(EnumType.STRING)
+    private WorkoutType type;
+}
--- src/main/java/com/example/demo/model/Exercise.java
+++ src/main/java/com/example/demo/model/Exercise.java
@@ -1,26 +1,11 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.Entity;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @Entity
-@Data
 @NoArgsConstructor
-public class Exercise {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Exclude
-    private Long id;
-    private String name;
-    private int sets;
-    private int reps;
-    private double weightIncrement;
-    private double initialWeight;
-
-    @Enumerated(EnumType.STRING)
-    private WorkoutType type;
+public class Exercise extends BaseExercise {
 }
